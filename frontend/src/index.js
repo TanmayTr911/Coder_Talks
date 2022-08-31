ea07007c3fd0f1a2b15a9b3a5a40fe3b55da1d23
Tanmay Raghuvanshi <89075656+TanmayTr911@users.noreply.github.com>
2022-08-31 15:13:26 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,7 +8,7 @@ import ChatProvider from "./Context/ChatProvider";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter basename='/'>
+  <BrowserRouter >
     <ChatProvider>
       <ChakraProvider>
         <App />
